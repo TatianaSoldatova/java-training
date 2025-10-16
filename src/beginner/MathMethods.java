71eba261b752fede59623a3b6752dd364c87aebd
Tatiana Soldatova <tatiana.soldatova2006@gmail.com>
2025-10-16 15:58:47 -0400

    added a new function to MathMethods

--- src/beginner/MathMethods.java
+++ src/beginner/MathMethods.java
@@ -6,11 +6,12 @@ public class MathMethods {
 
     public static void main() {
         thirdDegreePolynomial();
+        formulas();
         input.close();
     }
 
     /**
-     * Evaluates the third degree polynomials of this form:
+     * Evaluates the third degree polynomials of the form:
      * ((a+b)/2) * x^3 + (a+b)^2 * x^2 + a + b + c
      */
     public static void thirdDegreePolynomial(){
@@ -39,5 +40,58 @@ public class MathMethods {
         double polynomialValue = ((abSum / 2) * xPow3) + (abSumPow2 * xPow2) + a + b + c;
         System.out.println(MSG_RESULT + polynomialValue);
         System.out.println();
+   }
+
+    /**
+     * Determines if X is defined for each of the formulas and calculates the result
+     * Logarithm is defined for positive real numbers > 0
+     * Square root is defined for real numbers >= 0
+     * 1/x is defined for real numbers != 0
+     */
+   public static void formulas(){
+       final String MSG_REAL_NUMBER = "Enter a real number: ";
+       final String MSG_UNDEFINED   = "undefined";
+       final String MSG_FORMULA     = "Formula %d: %s %n";
+       final String MSG_ERR         = "Invalid input!";
+
+       double x = UserInputUtils.readDouble(input, MSG_REAL_NUMBER, MSG_ERR);
+
+       // formula 1: x / (1 - exp(x))
+       double result = 1 - Math.exp(x);
+       if(result == 0){
+           System.out.printf(MSG_FORMULA, 1, MSG_UNDEFINED);
+       }else{
+           result = x / result;
+           System.out.printf(MSG_FORMULA, 1, result);
+       }
+
+       // formula 2: x * log(x) * exp(2/(x-1))
+       if((x - 1) == 0 || x <= 0){
+           System.out.printf(MSG_FORMULA, 2, MSG_UNDEFINED);
+       }else{
+           result = x * Math.log(x) * Math.exp(2/(x - 1));
+           System.out.printf(MSG_FORMULA, 2, result);
+       }
+
+       // formula 3: (-x - sqrt(x^2 - 8 * x)) / (2 - x)
+       double underRoot = x * x - 8 * x;
+       if(underRoot < 0 || (2 - x) == 0){
+           System.out.printf(MSG_FORMULA, 3, MSG_UNDEFINED);
+       }else{
+           result = (-x - Math.sqrt(underRoot)) / (2 - x);
+           System.out.printf(MSG_FORMULA, 3, result);
+       }
+
+       // formula 4: sqrt((sin(x) - x/20) * log(sqrt(x^2 - (1/x)))
+       double underRoot1 = x * x - (1/x);
+       double underRoot2 = Math.sin(x) - (x/20);
+       double rootResult = Math.sqrt(underRoot1);
+       if(x == 0 || underRoot1 < 0 || underRoot2 < 0 || Math.log(rootResult) <= 0){
+           System.out.printf(MSG_FORMULA, 4, MSG_UNDEFINED);
+       }else{
+           result = Math.sqrt((Math.sin(x) - x/20) * Math.log(rootResult));
+           System.out.printf(MSG_FORMULA, 4, result);
+       }
+       System.out.println();
    }
 }
